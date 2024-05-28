0fe69debbaa87b8f373c878827e5ed818854e4d7
KoPhaiVu <hoangle293vu@gmail.com>
2024-05-28 20:34:30 +0700

    Commit to pull

--- src/app/src/main/java/com/example/app/activity/Activity_Add_Class.java
+++ src/app/src/main/java/com/example/app/activity/Activity_Add_Class.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -15,20 +17,29 @@ import com.example.app.R;
 import com.example.app.model.ClassDTO;
 
 public class Activity_Add_Class extends AppCompatActivity {
-    EditText classID, className, startDate, endDate, programID, teacherName, staffID;
+    EditText classID, className, startDate, endDate, idTeacher, staffID;
     Button exitBtn, doneBtn;
+    String[] roomIDItem = {"Hehe", "Hoho", "Huhu"};
+    String[] programIDItem = {"Hmmmm", "Oke", "Ờm Ờm"};
+    AutoCompleteTextView room, program;
+    ArrayAdapter<String> roomAdapter, programAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_class);
 
+        room = findViewById(R.id.roomID);
+        program = findViewById(R.id.programID);
+
+        roomAdapter = new ArrayAdapter<String>(this, R.layout.combobox_item, roomIDItem);
+        programAdapter = new ArrayAdapter<String>(this, R.layout.combobox_item, programIDItem);
+
         classID = findViewById(R.id.classID);
         className = findViewById(R.id.class_name);
         startDate = findViewById(R.id.start_date);
         endDate = findViewById(R.id.end_date);
-        programID = findViewById(R.id.programID);
-        teacherName = findViewById(R.id.teacher_name);
+        idTeacher = findViewById(R.id.idTeacher);
         staffID = findViewById(R.id.staffID);
 
         String message = getIntent().getStringExtra("classID");
@@ -57,9 +68,8 @@ public class Activity_Add_Class extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if (className.getText().toString().equals("") || classID.getText().toString().equals("")
-                        || startDate.getText().toString().equals("") || teacherName.getText().toString().equals("")
-                        || endDate.getText().toString().equals("") || programID.getText().toString().equals("")
-                        || staffID.getText().toString().equals("")) {
+                        || startDate.getText().toString().equals("") || idTeacher.getText().toString().equals("")
+                        || endDate.getText().toString().equals("") || staffID.getText().toString().equals("")) {
                     Toast.makeText(Activity_Add_Class.this, "All fields are mandatory", Toast.LENGTH_SHORT).show();
                 } else {
                     finish();
